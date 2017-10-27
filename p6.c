9de60c4972ac15342ad6bd611c854cf982e9eb46
tasnim5420 <32926013+tasnim5420@users.noreply.github.com>
2017-10-27 23:27:35 +0600

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+     for(i=n;i>=1;i--)
+    {
+
+        for(j=i;j<=n;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=(2*i-3);j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
